faeae6fc4db65579dbf37795b52925cf3765e910
HeckNoHey <Cartire64@gmail.com>
2026-01-07 14:32:42 -0600

    made basics

--- Super Boogle Bros/src/GamePanel.java
+++ Super Boogle Bros/src/GamePanel.java
@@ -1,5 +1,113 @@
-public class GamePanel {
-    public static void main(String args[]){
-        
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable
+{
+    
+    private Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
+
+    public GamePanel()
+    {
+        this.setPreferredSize(new Dimension((int)screenWidth, (int)screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+
+    }
+
+    public void startGameThread()
+    {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run()
+    {
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+/* 
+    public void startGameThread()
+    {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    double drawInterval = 1000000000.0 / FPS;
+    double delta;
+    long lastTime;
+    long currentTime;
+
+    @Override
+    public void run()
+    {
+        delta = 0;
+        lastTime = System.nanoTime();
+
+        while(gameThread != null)
+        {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta > 1)
+            {
+                update();
+                repaint();
+
+                delta--;
+            }
+        }
+    }
+
+    public void update()
+    {
+        player.update();
+        entityManager.update();
+        tileM.update();
+    }
+
+    @Override
+    public  void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        try
+        {
+            tileM.draw(g2);
+            player.draw(g2);
+            entityManager.draw(g2);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+
+        
+        g2.dispose();
+
+
+    }
+}
+*/
---
+++ Super Boogle Bros/src/Main.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String args[])
+    {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Test1");
+        
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+
+
+        gamePanel.startGameThread();
+    }
+}
