7c54939a2a5da772020ffe4f76b71d67f4cba2fd
HeckNoHey <Cartire64@gmail.com>
2026-01-09 13:59:26 -0600

    packaged2

--- Super Boogle Bros/src/client/Client.java
+++ Super Boogle Bros/src/client/Client.java
@@ -6,7 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ConnectException;
 import java.net.Socket;
-import src.packets.RemovePlayerPacket;
+import packets.RemovePlayerPacket;
 
 
 
