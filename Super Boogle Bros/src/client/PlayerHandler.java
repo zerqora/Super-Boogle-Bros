b033c42a203db57ff2febe2f7c53bb3cb4de7ae7
zerqora <angelduong0@gmail.com>
2026-01-09 14:04:12 -0600

    Update PlayerHandler.java

--- Super Boogle Bros/src/client/PlayerHandler.java
+++ Super Boogle Bros/src/client/PlayerHandler.java
@@ -3,7 +3,9 @@ package client;
 import java.util.HashMap;
 
 
-
+// Handles if the player joins or leaves
 public class PlayerHandler {
+    // ID, Player with their ID and name
+    // When a player joins the server it adds them to the hash map
     public static HashMap<Integer, NetPlayer> players = new HashMap<>();
 }
